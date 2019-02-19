e7af7d46e1b9b31659f63e19fce512cca89dde64
Gennadii Altukhov <grinrag@gmail.com>
2019-02-19 13:23:52 +0100

    add setup.py to build package.

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+
+setup(
+    name="blkinfo",
+    version="0.0.1",
+    author="Gennadii Altukhov",
+    author_email="galt@redhat.com",
+    description="Library for listing all block devices available in the system using additional filters",
+    license="GPLv3",
+    url="https://github.com/grinrag/disk_enumeration_poc",
+    packages=['blkinfo'],
+    package_dir={"": "src"},
+    keywords='lsblk disk blockdevice blockinfo blkinfo',
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Topic :: Software Development :: Libraries :: Python Modules"
+    ],
+)
