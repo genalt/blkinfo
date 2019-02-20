3736af794a93991920ca8673b9cdef6a4f5847a4
Gennadii Altukhov <grinrag@gmail.com>
2019-02-20 13:16:30 +0100

    bump version to build new python package and publish it to PyPI

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name="blkinfo",
-    version="0.0.2",
+    version="0.0.3",
     author="Gennadii Altukhov",
     author_email="galt@redhat.com",
     description="blkinfo is a python package to list information about all available or the specified block devices.",
